8aa9a1f8ebafb82ce61b8252ed7e630fba97758a
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2020-06-25 11:37:18 +0200

    checkpoint 3.3

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ let persons = [
         name: 'Jens Hansen',
         number: '423-278291',
         id: 3
+    },
+    {
+        name: 'Alex Sten',
+        number: '021-272399',
+        id: 4
+    },
+    {
+        name: 'Ole Dam',
+        number: '214-223891',
+        id: 5
     }
 ]
 
@@ -35,6 +45,18 @@ const info = () => {
 
 app.get('/', (request, response) => {
     response.send(info())
+})
+
+app.get('/api/persons/:id', (request, response) => {
+     const id = Number(request.params.id)
+     const person = persons.find(person => person.id === id)
+     
+     if (person) {
+        response.json(person)
+     } else {
+         response.status(404).end()
+     }
+     
 })
 
 const PORT = 3001

39d16739c4ad350dc9a743aea6d49ef84d152198
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2020-06-27 10:53:23 +0200

    checkpoint 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         name: 'Arto Hellas',
@@ -65,6 +67,33 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const generateId = () => {
+    const id = Math.floor(Math.random()*1000)
+    console.log(id);
+    return id
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body);
+    if(!body.name){
+        return response.status(400).json({
+            error: "content missing"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(persons)
+}) 
+
 
 const PORT = 3001
 app.listen(PORT, () => {
